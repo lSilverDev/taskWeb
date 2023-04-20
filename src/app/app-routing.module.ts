d26cedae39716f17ebdb1f72e0a5b5d0248323a4
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-04-20 19:50:08 -0300

    struct - done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { ProfileComponent } from './views/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/service/tasks.service.ts
+++ src/app/service/tasks.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { getFirestore, setDoc } from 'firebase/firestore';
 import { app } from 'src/environment/environment';
-import { task } from '../modal/task/task';
 
 @Injectable({
   providedIn: 'root'
