015a9a7e2ae71c53b93865547af8f539744726b0
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-04-20 19:44:51 -0300

    infrastructure - GCP

--- src/app/service/tasks.service.ts
+++ src/app/service/tasks.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { getFirestore, setDoc } from 'firebase/firestore';
+import { app } from 'src/environment/environment';
+import { task } from '../modal/task/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
+  db = getFirestore(app);
+
   constructor() { }
+
 }
---
+++ src/environment/environment.ts
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  // informaçoes de coneçao do projeto aqui
+};
+
+export const app = initializeApp(firebaseConfig);
