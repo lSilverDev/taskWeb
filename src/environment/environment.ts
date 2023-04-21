1ff1a44546ec708f0793999ac659f1694d8404cf
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-04-21 11:32:19 -0300

    enviroment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  // informaçoes de coneçao do projeto aqui
+  apiKey: "AIzaSyDukB5IVYVgFZMZVrqq_A23ulOz-9Ld6Hg",
+  authDomain: "taskweb-1adc2.firebaseapp.com",
+  projectId: "taskweb-1adc2",
+  storageBucket: "taskweb-1adc2.appspot.com",
+  messagingSenderId: "424819929314",
+  appId: "1:424819929314:web:c193404a4c4d304cc06ebb"
 };
 
 export const app = initializeApp(firebaseConfig);
