edbe7a4764a23f4bc5e38431378b431cb5382bfa
Diego Gomes <129207541+Diego-Gomes84@users.noreply.github.com>
2023-04-22 14:36:13 -0300

    adding logo imgs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProfileComponent } from './views/profile/profile.component';
 import { TaskCardComponent } from './components/task-card/task-card.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { LoginComponent } from './views/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { LoginComponent } from './views/login/login.component';
     ProfileComponent,
     TaskCardComponent,
     SideBarComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
